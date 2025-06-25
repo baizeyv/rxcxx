fc5c845375ed9169a239faf321e53177db69a591
baizeyu <baizeyu@doodlemobile.com>
2025-06-25 12:42:28 +0800

    tmp

---
+++ src/extra/int_message.h
@@ -0,0 +1,15 @@
+﻿//
+// Created by baizeyv on 6/24/2025.
+//
+
+#ifndef INT_MESSAGE_H
+#define INT_MESSAGE_H
+#include <unordered_map>
+
+#include "simple_event.hpp"
+
+class int_message {
+private:
+};
+
+#endif //INT_MESSAGE_H
